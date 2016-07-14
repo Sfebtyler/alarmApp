42a9ce03356b57321a75fa4ce03d784876b41c8c
Eric Belliston <ericbelliston@Erics-MacBook-Pro.local>
2016-07-14 16:28:44 -0600

    changed datetimepicker

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ var config = {
 			'node_modules/bootstrap/dist/css/bootstrap.min.css',
 			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
 			'node_modules/toastr/build/toastr.css',
+			'node_modules/react-date-picker/index.css',
 			'client/src/style.css'
 		],
 		dist: './dist'
