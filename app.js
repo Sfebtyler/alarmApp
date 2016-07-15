fd3ccaf962b7dd8ff5f2ad70ef0b4badfd612108
Eric Belliston <ericbelliston@Erics-MacBook-Pro.local>
2016-07-15 13:57:42 -0600

    fixing some stuff and yeah

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var skipper = require('skipper');
 
 
 //Configure Mongoose
-mongoose.connect('mongodb://mongodb.cs.dixie.edu/EricBelliston')
+// mongoose.connect('mongodb://mongodb.cs.dixie.edu/EricBelliston')
+mongoose.connect('mongodb://localhost/EricBelliston')
 
 mongoose.connection.on('connected', function() {
 	console.log("Data Base Connected...\n".green);
