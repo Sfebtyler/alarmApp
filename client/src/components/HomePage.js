18e491d77b127e7a4b551f760494c5b2cef2b5ef
Jeanette Stander 4H <jstander@plutonium.cs.dixie.edu>
2016-07-18 17:22:29 -0600

    fixed delete and working on sorting lists

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,10 +7,13 @@ var OverdueList = require('./reminders/overdueList');
 var AddButton = require('./reminders/button');
 var ReminderStore = require('../stores/reminderStore');
 var Link = require('react-router').Link;
+var Moment = require('moment');
+
 var Home = React.createClass({
   getInitialState: function(){
     return{
-      reminders: ReminderStore.getAllReminders()
+      reminders: ReminderStore.getAllReminders(),
+      //todayReminders: this.getTodayReminders()
     }
   },
   componentWillMount: function(){
@@ -18,12 +21,32 @@ var Home = React.createClass({
   },
   componentWillUnmount: function(){
     ReminderStore.removeChangeListener(this.onChange);
+
   },
   onChange: function(){
     this.setState({
-      reminders: ReminderStore.getAllReminders()
-    });
+      reminders: ReminderStore.getAllReminders(),
+     // todayReminders: this.getTodayReminders()
+  	});
   },
+  getTodayReminders: function(){
+  	console.log("is this working?")
+  	var todayReminders = [];
+  	console.log(this.state.reminders)
+  	if (this.state.reminders != null){
+	    for (var i; i<this.state.reminders.length; i++){
+	    	reminder = this.state.reminders[i];
+	    	console.log(reminder)
+	    	if (Moment(reminder.dueDate).format('MMMM Do YYYY') === Moment(Date.now()).format('MMMM Do YYYY')){
+	    		todayReminders.push(reminder)
+	    		console.log(reminder)
+	    	}
+	    }
+	    console.log(todayReminders)
+	    return todayReminders
+	}
+  },
+
 	render: function () {
 		return (
 			<div>
@@ -32,7 +55,7 @@ var Home = React.createClass({
 						reminders={this.state.reminders}
 					/>
 					<RemindersList 						
-						reminders={this.state.reminders}
+						reminders={this.getTodayReminders()}
 					/>
 					<OverdueList 
 						reminders={this.state.reminders}
