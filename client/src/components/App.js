e533dafb12ffd8c5eb618dba974a89bc41427d56
sstander <sarahtstander@gmail.com>
2016-07-20 15:01:22 -0600

    Header better

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,14 @@ var App = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<div><h1>Memorable Memos</h1></div>
+				<div className="row">
+					<div id="memoLogo" className="col-sm-1">
+						<img  src="../images/Memo.png" alt="memoLogo" width="75" height="75"/>
+					</div>
+					<div className="col-sm-9">
+						<h1 >Memorable Memos</h1>
+					</div>
+				</div>
 				<Header />
 				<div className="container">
 					{this.props.children}
