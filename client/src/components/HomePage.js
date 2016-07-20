cfbec0cbbdec3e62604edf1ee3bf43771321ec5c
Jeanette Stander 4H <jstander@plutonium.cs.dixie.edu>
2016-07-20 16:31:26 -0600

    fixed updating state on homepage and also some styling

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -12,64 +12,58 @@ var Moment = require('moment');
 var Home = React.createClass({
   getInitialState: function(){
     return{
-      reminders: ReminderStore.getAllReminders()
+      reminders: ReminderStore.getAllReminders(),
+      todayReminders: ReminderStore.getAllTodayReminders(),
+      overdueReminders: ReminderStore.getAllOverdueReminders(),
+      upcomingReminders: ReminderStore.getAllUpcomingReminders()
     }
+  },
+  componentDidMount: function () {
+    var newReminders = [];
+    this.timerInterval = setInterval(this.functionToBeCalled, 1000);
+  },
+  functionToBeCalled: function(){
+    var newTodayReminders = ReminderStore.getAllTodayReminders();
+    var newOverdueReminders = ReminderStore.getAllOverdueReminders();
+    var newUpcomingReminders = ReminderStore.getAllUpcomingReminders();
+    this.setState({
+      todayReminders: newTodayReminders,      
+      overdueReminders: newOverdueReminders,
+      upcomingReminders: newUpcomingReminders
+    });
   },
   componentWillMount: function(){
     ReminderStore.addChangeListener(this.onChange);
   },
+
   componentWillUnmount: function(){
     ReminderStore.removeChangeListener(this.onChange);
+    clearInterval(this.timerInterval);
 
   },
+
   onChange: function(){
     this.setState({
       reminders: ReminderStore.getAllReminders(),
+      todayReminders: ReminderStore.getAllTodayReminders(),
+      overdueReminders: ReminderStore.getAllOverdueReminders(),
+      upcomingReminders: ReminderStore.getAllUpcomingReminders()
   	});
   },
-  getTodayReminders: function(){
-  	var todayReminders = [];
-    for (var i = 0; i<this.state.reminders.length; i++){
-    	var reminder = this.state.reminders[i];
-    	if (Moment(reminder.dueDate).format('MMMM Do YYYY HH mm') >= Moment(Date.now()).format('MMMM Do YYYY HH mm') && Moment(reminder.dueDate).format('MMMM Do YYYY') === Moment(Date.now()).format('MMMM Do YYYY') && reminder.completed === false){
-    		todayReminders.push(reminder)
-    	}
-    }
-    return todayReminders
-  },
-  getOverdueReminders: function(){
-  	var OverdueReminders = [];
-    for (var i = 0; i<this.state.reminders.length; i++){
-    	var reminder = this.state.reminders[i];
-    	if (Moment(reminder.dueDate).format('MMMM Do YYYY HH mm') < Moment(Date.now()).format('MMMM Do YYYY HH mm') && reminder.completed === false){
-    		OverdueReminders.push(reminder)
-    	}
-    }
-    return OverdueReminders
-  },
-  getUpcomingReminders: function(){
-  	var UpcomingReminders = [];
-    for (var i = 0; i<this.state.reminders.length; i++){
-    	var reminder = this.state.reminders[i];
-    	if (Moment(reminder.dueDate).format('MMMM Do YYYY') > Moment(Date.now()).format('MMMM Do YYYY') && reminder.completed === false){
-    		UpcomingReminders.push(reminder)
-    	}
-    }
-    return UpcomingReminders
-  },
+
 	render: function () {
 		return (
 			<div>
 				<div className="row">
 					<UpcomingList 						
-						reminders={this.getUpcomingReminders()}
+						reminders={this.state.upcomingReminders}
 					/>
 					<RemindersList 						
-						reminders={this.getTodayReminders()}
+						reminders={this.state.todayReminders}
 						// onReminder={this.onChange}
 					/>
 					<OverdueList 
-						reminders={this.getOverdueReminders()}
+						reminders={this.state.overdueReminders}
 					/>
 				</div>
 				<Link to='/reminders-page'><AddButton /></Link>
