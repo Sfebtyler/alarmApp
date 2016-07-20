83faa86fe6110e0101348046faad98a61e7102b5
sstander <sarahtstander@gmail.com>
2016-07-20 16:37:30 -0600

    added links to header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var Header = require('./common_2/Header');
+var Link = require('react-router').Link;
 
 
 var App = React.createClass({
@@ -9,12 +10,16 @@ var App = React.createClass({
 		return (
 			<div>
 				<div className="row">
+					<Link to = '/'>
 					<div id="memoLogo" className="col-sm-1">
 						<img  src="../images/Memo.png" alt="memoLogo" width="75" height="75"/>
 					</div>
+					</Link>
+					<Link to = "/">
 					<div className="col-sm-9">
 						<h1 >Memorable Memos</h1>
 					</div>
+					</Link>
 				</div>
 				<Header />
 				<div className="container">
